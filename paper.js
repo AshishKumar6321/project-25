55290ee466b758ec2c967ef6a31cad42abb8841f
AshishKumar6321 <76861673+AshishKumar6321@users.noreply.github.com>
2021-02-16 14:38:32 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,14 @@
+class Paper{
+    constructor(x,y){
+    var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+    this.image=loadImage("paper.png");
+    if(keyDown(RIGHT_ARROW)){
+        paper.x=650;
+    }
+}
+};
